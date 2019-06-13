ca42dfd3923bcc2ebceda94b21f51128080ba6a6
Eugene Korniets <evgeniy.korniets@gmail.com>
2019-06-13 22:57:34 +0300

    add plugins

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,12 +4,12 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: colors.blue.darken2,
+    primary: '#4F3EC8',
     accent: colors.grey.darken3,
     secondary: colors.amber.darken3,
     info: colors.teal.lighten1,
     warning: colors.amber.base,
-    error: colors.deepOrange.accent4,
+    error: '#E53935',
     success: colors.green.accent3
   }
 })
