5f6b0dda4610e7f2529a874c2c4c7a95908900fc
Eugene Korniets <evgeniy.korniets@gmail.com>
2019-06-13 22:57:16 +0300

    add store

---
+++ store/index.js
@@ -0,0 +1,22 @@
+export const state = () => ({
+  user: {},
+
+  messages: []
+})
+
+export const mutations = {
+  setUser (state, user) {
+    state.user = user
+  },
+
+  clearData (state) {
+    state.user = {}
+    state.messages = []
+  }
+}
+
+export const actions = {
+  SOCKET_newMessage (context, data) {
+    console.log('Message received ', data)
+  }
+}
